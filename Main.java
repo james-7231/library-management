e3a6c366186b46afdf334574bce05fc0767c88ea
James Hill <JamesTristanHill1998@gmail.com>
2026-07-12 17:51:03 -0500

    reoganized the main, and added branches to start creating classes

--- Main.java
+++ Main.java
@@ -2,43 +2,7 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args){
-            ///Creates a new scanner to ask for the name.
-        Scanner input = new Scanner(System.in);
-            ///Sends console log to prompt user for their name.
-        System.out.print("What's your name: ");
-            ///Collects the users name
-        String name = input.nextLine();
-            ///Prints the users name to the console.
-        System.out.printf("Welcome to GitHub, %s!", name);
-            ///Closes the scanner.
-        System.out.println();
-        System.out.print("How old are you?: ");
 
-        int age = input.nextInt();
-        System.out.println();
-        System.out.printf("%s, you're %d years old!", name, age);
-        System.out.print("Would you like to make an account (y/n) : ");
-        System.out.println();
-        System.out.print("What is your code");
-        int code = input.nextInt();
-        System.out.println(code);
-        if (isInt(code)){
-            System.out.println("Thanks for the code");
-        }
-
-        input.close();
-
+    
 
     }
-    //does literally nothing useful, just practicing making functions again
-    public static boolean isInt(int x){
-        if (x>0){
-            return true;
-        }
-        return false;
-    }
-
-
-
-}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# git-practice
+# this is my first application
+#
+# It's a library management system with the features: 
+#
+#   
